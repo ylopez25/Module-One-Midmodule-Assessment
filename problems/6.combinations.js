bff941316831941122b8376e72d151e99e31eadb
Ben Stone <benjamin.h.stone@gmail.com>
2020-10-09 17:46:26 -0400

    update method name

--- problems/6.combinations.js
+++ problems/6.combinations.js
@@ -10,16 +10,22 @@ function removeOddNumbers() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - An array removing all elements initially appearing at an odd index
 */
-function removeValuesAtOddIndices() {
+function removeNumbersAtOddIndices() {
 
 }
 
-/** Write a function that returns all the values from 1 to n inclusive, replacing all numbers that are a multiple of seven, or contain seven with "BOOM".  If a number is both a multiple of seven, and contains a seven, replace it with "BOOMBOOM" instead.
+/** Write a function that returns all the values from 1 to n inclusive, replacing all numbers that are a multiple of seven, or contain seven with "BOOM".
 * Sample Input: 20
-* Sample Output: [1,2,3,4,5,6,BOOMBOOM,8,9,10,11,12,13,BOOM,15,16,BOOM,18,19,20]
+* Sample Output: [1,2,3,4,5,6,BOOM,8,9,10,11,12,13,BOOM,15,16,BOOM,18,19,20]
 * @param {number} n - The number to count up to
 * @returns {number[]} - An array matching the pattern described above
 */
 function sevenBoom() {
-  
+
+}
+
+module.exports = {
+  removeOddNumbers,
+  removeNumbersAtOddIndices,
+  sevenBoom
 }
