dfdfcfc850d099f2f1709002f8aa0fff1bcb73f0
Corey Ladovsky <coreyskymusic@gmail.com>
2020-12-21 13:22:44 -0800

    clearer instructions on loops

--- problems/5.loops.js
+++ problems/5.loops.js
@@ -7,6 +7,7 @@ function evenNumbers() {
 }
 
 /** Returns all multiples of a given number up to 1000
+ * Example if num was 3 the output would be [0, 3, 6, 9, 12, ...etc ] until 1000
 * @param {number} num - The number to find multiples of
 * @returns {number[]} - An array of all multiples of {num} up to 1000
 */
@@ -17,6 +18,7 @@ function multiplesToOneThousand() {
 /** Returns the first n powers of 2 (starting at 0)
 * Sample input: 6
 * Sample output: [1,2,4,8,16,32]
+* This is because 2^0 === 1 , 2^1 === 2, 2^2 === 4, 2^3 === 8, etc...
 * @param {number} count - How many powers of two to find
 * @returns {number[]} - An array with the first {count} powers of two
 */
